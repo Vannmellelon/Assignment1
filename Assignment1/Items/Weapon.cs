6f9596fcd974e7aa8a1c6ce15306284e0a512db8
Anne E. Larsen <anneelarsen98@gmail.com>
2022-01-21 00:44:03 +0100

    Made item classes

---
+++ Assignment1/Items/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1.Items
+{
+    class Weapon : Item
+    {
+        public enum WeaponType
+        {
+            Axe,
+            Bow,
+            Dagger,
+            Hammer,
+            Staff,
+            Sword,
+            Wand
+        }
+
+        public WeaponType Type { get; set; }
+    }
+}
